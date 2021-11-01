0e9d89ee712e825bc7edd0ca1939827426ec7578
AbhijithNT <abhijithnt64@gmail.com>
2021-11-01 23:58:58 +0530

    config

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,6 @@
 from re import I
 from bot import BOT_TOKEN
+from .plugins import income_text_handler, tr_command
 from .plugins import start_msg
 from telegram.ext import (
     Updater,
@@ -13,5 +14,14 @@ if __name__ == '__main__':
     updater = Updater(BOT_TOKEN)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start_msg))
+    # tr
+    dispatcher.add_handler(
+        CommandHandler("tr", tr_command,
+                       Filters.text & Filters.reply & Filters.group)
+    )
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command & Filters.reply,
+                       income_text_handler)
+    )
     updater.start_polling()
     updater.idle()
