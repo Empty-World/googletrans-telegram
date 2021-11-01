ad4aec8d25430a5ac483df95961630834af3a477
AbhijithNT <abhijithnt64@gmail.com>
2021-11-01 23:59:29 +0530

    tr_command()

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,7 +1,20 @@
 from telegram import Update
+from ..utils import google_tr
+from googletrans import LANGUAGES
 
-def start_msg(update: Update, _) ->None:
+
+def start_msg(update: Update, _) -> None:
     """
     Replies to /start command  
     """
+    print(update.message)
     update.message.reply_text("**Hello World**",)
+
+
+def tr_command(update: Update, _) -> None:
+    lang_code = update.message.text.split(" ")[-1]
+    if lang_code in LANGUAGES:
+        result = google_tr(update.message.reply_to_message.text, lang_code)
+        update.message.reply_text(result)
+    else:
+        update.message.reply_text('Language not supported')
