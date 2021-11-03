daa43042392a09f6e1c807c199ba5975a5f8737e
AbhijithNT <abhijithnt64@gmail.com>
2021-11-03 19:16:48 +0530

    Improved performance and added a comment to the tr_command function

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,17 +1,21 @@
-from telegram import Update
+from telegram import Update, ForceReply
 from ..utils import google_tr
 from googletrans import LANGUAGES
 
 
 def start_msg(update: Update, _) -> None:
-    """
-    Replies to /start command  
-    """
-    print(update.message)
-    update.message.reply_text("**Hello World**",)
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}!\n',
+        reply_markup=ForceReply(selective=True),
+    )
 
 
 def tr_command(update: Update, _) -> None:
+    """
+    Sends a message when the /tr command is entered.
+    """
     lang_code = update.message.text.split(" ")[-1]
     if lang_code in LANGUAGES:
         result = google_tr(update.message.reply_to_message.text, lang_code)
