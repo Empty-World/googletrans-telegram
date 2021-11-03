aa368e49cb966b4d1e7361134deef42c156f714b
AbhijithNT <abhijithnt64@gmail.com>
2021-11-03 20:22:41 +0530

    logger

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,35 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from .env import get_env
+
 
 __version__ = "0.1.1"
 __author__ = "Abhijith N T"
 
 BOT_TOKEN = get_env('BOT_TOKEN')
+
+
+START_MESSAGE = "*It is the telegram bot that uses to translate messages.*"
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            'tr_bot_log.txt',
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("telegram").setLevel(logging.WARNING)
+
+
+def logger(logs: str) -> logging.Logger:
+    """
+    logger function  
+    """
+    return logging.getLogger(logs)
